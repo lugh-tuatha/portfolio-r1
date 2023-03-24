2ebee3952f1e1dcb9411f808a9b9741e17d4698e
Ace <aceonsteam001@gmail.com>
2023-03-24 08:22:55 +0800

    fix: navbar responsiveness

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,34 +2,49 @@ import React, { useState } from 'react'
 import './navbar.css'
 
 import * as Hi from "react-icons/hi2";
+import * as Fa from "react-icons/fa";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <div className='container mx-auto '>
+    <div className='container mx-auto'>
       <nav className='justify-between mt-6 hidden sm:flex'>
         <h1 className='self-center'>LOGO</h1>
 
         <ul className='flex gap-10'>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>home</li>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>works</li>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>about me</li>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>contacts</li>
+          <li className='text-lg font-light text-gray'><a href='#home'><span className='text-primary font-bold'>#</span>home</a></li>
+          <li className='text-lg font-light text-gray'><a href='#works'><span className='text-primary font-bold'>#</span>works</a></li>
+          <li className='text-lg font-light text-gray'><a href='#about'><span className='text-primary font-bold'>#</span>about me</a></li>
+          <li className='text-lg font-light text-gray'><a href='#contacts'><span className='text-primary font-bold'>#</span>contacts</a></li>
         </ul>
       </nav>
 
       <nav className='flex sm:hidden justify-between mx-4 mt-6'>
         <h1 className='self-center'>LOGO</h1>
-        
-        <Hi.HiBars3BottomRight size={30}/>
 
-        <ul className='hidden'>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>home</li>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>works</li>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>about me</li>
-          <li className='text-lg font-light text-gray'><span className='text-primary font-bold'>#</span>contacts</li>
+        <button>
+          <Hi.HiBars3BottomRight size={30} onClick={toggleMenu} />
+        </button>
+
+        <ul className={`absolute top-16 left-4 w-11/12 ${isMenuOpen ? 'block' : 'hidden'}`}>
+          <li><a href='#home' onClick={toggleMenu} className='font-light text-gray text-2xl'><span className='text-primary font-bold'>#</span>home</a></li>
+          <li className='mt-2'><a href='#works' onClick={toggleMenu} className='font-light text-gray text-2xl'><span className='text-primary font-bold'>#</span>works</a></li>
+          <li className='mt-2'><a href='#about' onClick={toggleMenu} className='font-light text-gray text-2xl'><span className='text-primary font-bold'>#</span>about me</a></li>
+          <li className='mt-2'><a href='#contacts' onClick={toggleMenu} className='font-light text-gray text-2xl'><span className='text-primary font-bold'>#</span>contacts</a></li>
+        
+        <div className='flex justify-center'>
+          <Fa.FaGithubSquare size={36} className="mt-4 text-gray cursor-pointer"/>
+          <Fa.FaLinkedin size={36} className="mt-4 text-gray cursor-pointer"/>
+        </div>
+
         </ul>
+
       </nav>
-    
     </div>
   )
 }
